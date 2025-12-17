eaad11bb668304648da778e7d6b11001c494baf5
vincev20 <vincev20@yahoo.com>
2025-12-17 16:08:13 +0800

    Update App.java 2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,3 @@
-//package com.mycompany.app;
-
-/**
- * Hello world!
- *
- */
-
-/*public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World 3!" );
-        System.out.println( "Hello World 3!" );
-        System.out.println( "Hello World 3!" );
-    }
-}
-*/
-
 package com.mycompany.app;
 
 /** Prints a few lines to standard output. */
